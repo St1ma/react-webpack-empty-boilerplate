ad3bb586de4bafda33d55f3779a98f8d5a06342f
AnastasiiaK <kranchenko.anastasiia@gmail.com>
2016-08-03 08:24:30 +0300

    toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React                       from 'react';
+import { default as TouchBackend } from 'react-dnd-touch-backend';
+import { DragDropContext }         from 'react-dnd';
+
+import Buttons from 'ui-toolkit/lib/buttons';
 
 import './App.css';
 
-export default class App extends React.Component {
+class App extends React.Component {
     render() {
         return (
             <div className='App'>
                 Hello TM!
+                <Buttons.BF1
+                    label = 'You did it!'
+                />
             </div>
         );
     }
 }
+
+export default DragDropContext(TouchBackend)(App);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import React                from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './App.jsx';
+
+import 'ui-toolkit/css/main.css';
+
+injectTapEventPlugin();
 
 require('babel-polyfill');
 
